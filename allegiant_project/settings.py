b1009913c38b6cd01995b42b61318f1e52951972
Peter Flynn <fredoflynn@gmail.com>
2016-06-13 23:34:53 -0700

    prepare for heroku

--- allegiant_project/settings.py
+++ allegiant_project/settings.py
@@ -125,7 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static')
+)
 
 REST_FRAMEWORK = {
     'PAGE_SIZE': 20,
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from customer_browser.models import Customer
+from django.core.urlresolvers import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class CustomerTests(APITestCase):
+
+    def test_list_customers(self):
+        url = reverse('list_customers')
+        response = self.client.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
